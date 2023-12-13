0cf73020f510b7cc295dc24f70cedb9e23110a95
afg9327 <93969640+afg9327@users.noreply.github.com>
2023-12-13 14:47:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/s23w1501layout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.s23w1501layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.s23w1501layout.ui.theme.S23W1501LayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen()
+{
+    S23W1501LayoutTheme {
+        SongDrawer()
+    }
+}
